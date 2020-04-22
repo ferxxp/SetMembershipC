d4f38cf61e958faeeefe22007bdb52a4fa2dd647
ferxxp <nandoquev@gmail.com>
2020-04-22 13:28:11 +0200

    just a bit more

--- SM.cpp
+++ SM.cpp
@@ -4,15 +4,13 @@
 #include <numeric>
 #include <vector>
 #include <cmath>
-#include "DataTest/var.cpp"
 #include <limits>
 
 
 using namespace std;
 
 
-class Geeks
-{
+class Geeks{
   // Access specifier
 public:
 
@@ -77,37 +75,3 @@ public:
   }
 
 };
-
-
-
-
-int main () {
-
-  ofstream myfile;
-  myfile.open ("example.txt");
-  myfile << "Writing this to a file.\n";
-  myfile.close();
-
-  Geeks gee;
-
-  std::vector<double> vin1 (std::begin(v1), std::end(v1));
-
-  std::vector<double> vin2 (std::begin(v2), std::end(v2));
-
-  std::vector<double> vin3 (std::begin(v3), std::end(v3));
-
-  std::vector<std::vector<double> > vin;
-  vin.push_back(vin1);
-  vin.push_back(vin2);
-  vin.push_back(vin3);
-
-  std::vector<double> series (std::begin(y12), std::end(y12));
-
-
-  gee.calculateGamma(vin, series);
-  std::cout << gee.Gamma << '\n';
-
-  std::vector<double> VAL(std::begin(uval),std::end(uval));
-  std::cout << gee.SetMemebership(VAL);
-  return 0;
-}
