39330cf5e0105c3b4785e0adc7586e5a2a883305
ferxxp <nandoquev@gmail.com>
2020-04-23 02:05:15 +0200

    Data retieval from file created

--- SM.cpp
+++ SM.cpp
@@ -20,41 +20,46 @@ public:
   double Gamma;
   std::vector<std::vector<double> > U;
   std::vector<double> Y;
+  double error=0;
+  std::vector<std::vector<int> > Regresors;
+
 
   // Member Functions()
-  void calculateGamma(const std::vector<std::vector<double> > Vin,const std::vector<double> Yout){
+  void addData(const std::vector<std::vector<double> > Vin,const std::vector<double> Yout){
     U=Vin;
     Y=Yout;
+  }
+  void calculateGamma(void){
 
-    for (size_t i = 0; i < Yout.size(); i++) {
-      double error=0.05;
+    for (size_t i = 0; i < Y.size(); i++) {
       double Gamma_prob=0;
       std::vector<double> Current;
-      //std::cout<<endl;
-      for (size_t t = 0; t < Vin.size(); t++) {
-        Current.push_back(Vin[t][i]);
-        //std::cout << Vin[t][i]<<" ";
+
+      for (size_t t = 0; t < U.size(); t++) {
+        Current.push_back(U[t][i]);
+        //std::cout << U[t][i]<<" ";
       }
       //std::cout<<endl;
-      for (size_t j = i+1; j < Yout.size(); j++) {
+      for (size_t j = i+1; j < Y.size(); j++) {
         double Norma=0;
-        for (size_t k = 0; k < Vin.size(); k++) {
-          Norma+=std::pow(Vin[k][j]-Current[k],2);
-          //std::cout << Vin[k][j]<<" ";
+        for (size_t k = 0; k < U.size(); k++) {
+          Norma+=std::pow(U[k][j]-Current[k],2);
+          //std::cout << U[k][j]<<" ";
         }
-        Gamma_prob=(std::abs(Yout[i]-Yout[j])-error)/std::sqrt(Norma);
+        Gamma_prob=(std::abs(Y[i]-Y[j])-error)/std::sqrt(Norma);
 
         if(Gamma_prob>Gamma) Gamma=Gamma_prob;
-        // if(Gamma_prob>1.7) {
-        //   std::cout<< endl << j <<"-"<< i <<"norma"<< std::sqrt(Norma) <<"dif" << std::abs(Yout[i]-Yout[j])<<endl;
-        //   Norma=0;
-        //   for (size_t k = 0; k < Vin.size(); k++) {
-        //     std::cout << Vin[k][j]<<" "<< Current[k]<<" ";
-        //     Norma+=std::pow(Vin[k][j]-Current[k],2);
-        //     std::cout << std::pow(Vin[k][j]-Current[k],2)<<" = "<<Norma<<endl;
-        //   }
-          //std::cout<<Gamma<<endl;
-        //}
+
+        if(Gamma_prob>112.78) {
+          std::cout<< endl << j <<" - "<< i <<" norma "<< std::sqrt(Norma) <<" dif " << std::abs(Y[i]-Y[j])<<endl;
+          Norma=0;
+          for (size_t k = 0; k < U.size(); k++) {
+            std::cout << U[k][j]<<" "<< Current[k]<<" ";
+            Norma+=std::pow(U[k][j]-Current[k],2);
+            std::cout << std::pow(U[k][j]-Current[k],2)<<" = "<<Norma<<endl;
+          }
+          std::cout<<Gamma<<endl;
+        }
       }
 
     }
@@ -74,35 +79,114 @@ public:
     }
     return (Yupper+Ylower)/2;
   }
-  void readDataSet(const std::string dir,int nVar){
-    //std::cout << "Something" << '\n';
+
+  std::vector<std::vector<double> > readDataSet(const std::string dir,int nVar,int quant){
+    std::vector<std::vector<double> > DataSet;
     string line;
     ifstream myfile;
     myfile.open (dir);
     if (myfile.is_open())
-  {
-    U.clear();
-    for (size_t j = 0; j <= nVar; j++) {
-       std::vector<double> V;
-       U.push_back(V);
-    }
-    std::cout << int(U.size()) << '\n';
-    while ( getline (myfile,line) )
     {
-      std::istringstream ss( line );
-
+      DataSet.clear();
       for (size_t j = 0; j <= nVar; j++) {
-        std::string s;
-        if (!getline( ss, s, ',' )) break;
-        U[j].push_back(std::stod(s));
+        std::vector<double> V;
+        DataSet.push_back(V);
       }
+      long long int lines_read;
+      while ( getline (myfile,line) && lines_read< quant)
+      {
+        std::istringstream ss( line );
+
+        for (size_t j = 0; j <= nVar; j++) {
+          std::string s;
+          if (!getline( ss, s, ',' )) break;
+          DataSet[j].push_back(std::stod(s));
+        }
+        lines_read++;
+      }
+      myfile.close();
+
+
+
     }
-    myfile.close();
+    else cout << "Unable to open file";
+    std::cout << int(DataSet.size()) << '\n';
+    //DataSet.erase(DataSet.begin());
+    std::cout << int(DataSet.size()) << '\n';
+
+    for(auto a: DataSet){
+      // for(auto b: a){
+      //  std::cout << b ;
+      // }
+      std::cout << a[15849] << '\n';
+      std::cout << a[16034] << '\n';
+      std::cout << '\n' ;
+    }
+    return DataSet;
   }
-  else cout << "Unable to open file";
-  for(auto a: U[0]){
-    std::cout << a << '\n';
+  void setRegresor(std::vector<std::vector<int> > Reg){
+    Regresors=Reg;
+
   }
+  void setRegresor(std::vector<std::vector<int> > Reg, double e){
+    error=e;
+    Regresors=Reg;
+
+  }
+  void prepareData(std::vector<std::vector<double> > DataSet){
+    std::cout <<'a';
+    size_t size=DataSet[0].size();
+
+    long int numberIns=0;
+    for(auto b:Regresors) numberIns+=b[0];
+
+    for (size_t i = 0; i < numberIns; i++) {
+      std::vector<double> a;
+      U.push_back(a);
+      std::cout << int(U.size()) << '\n';
+    }
+    std::vector<int> sumRegresors;
+
+    std::cout <<'a';
+    for (auto Variable :Regresors)
+    sumRegresors.push_back(std::accumulate(Variable.begin(), Variable.end(), 0));
+    int initTimeStamp = *std::max_element(std::begin(sumRegresors), std::end(sumRegresors));
+
+    std::cout <<'\n';
+    std::cout<< initTimeStamp << '\n';
+    for (size_t timestamp = initTimeStamp; timestamp <= size; timestamp++) {
+      long int nVar=0, row=0;
+      for (auto variable:Regresors) {
+        for (size_t nV = 0; nV < variable[0]; nV++) {
+          U[row].push_back(DataSet[nVar][timestamp-variable[1]-nV]);
+          row++;
+        }
+        nVar++;
+      }
+      Y.push_back(DataSet[nVar-1][timestamp]);
+    }
+    std::cout << "/* message */" << '\n';
+
+    for(auto a: U){
+      int first100=0;
+      for(auto b: a){
+        std::cout << b <<", ";
+        if(first100>10) break;
+        first100++;
+      }
+      std::cout << endl <<endl;
+    }
+
+    std::cout << "OUT: ";
+    int first100=0;
+    for(auto a: Y){
+      std::cout << a <<", ";
+      if(first100>10) break;
+      first100++;
+    }
+    std::cout << endl;
+    std::cout <<U.size()<< endl;
+
   }
 
 };
--- Test1.cpp
+++ Test1.cpp
@@ -12,28 +12,35 @@
 using namespace std;
 
 int main () {
+  std::vector<std::vector<int> > REGS;
+
+  int v0[] = {1,0};
+  std::vector<int> a (std::begin(v0),std::end(v0));
+  REGS.push_back(a);
+  int v1[] = {3,8};
+  a=std::vector<int> (std::begin(v1),std::end(v1));
+  REGS.push_back(a);
+  int v2[] = {3,8};
+  a=std::vector<int> (std::begin(v2),std::end(v2));
+  REGS.push_back(a);
+  int v3[] = {3,8};
+  a=std::vector<int> (std::begin(v3),std::end(v3));
+  REGS.push_back(a);
+  int v4[] = {0,0};
+  a=std::vector<int> (std::begin(v4),std::end(v4));
+  REGS.push_back(a);
+  int y1[] = {3,1};
+  a=std::vector<int> (std::begin(y1),std::end(y1));
+  REGS.push_back(a);
 
   Geeks gee;
-
-  std::vector<double> vin1 (std::begin(v1), std::end(v1));
-
-  std::vector<double> vin2 (std::begin(v2), std::end(v2));
-
-  std::vector<double> vin3 (std::begin(v3), std::end(v3));
-
-  std::vector<std::vector<double> > vin;
-  vin.push_back(vin1);
-  vin.push_back(vin2);
-  vin.push_back(vin3);
-
-  std::vector<double> series (std::begin(y12), std::end(y12));
-
-
-  gee.calculateGamma(vin, series);
+  gee.setRegresor(REGS,0.5);
+  gee.prepareData(gee.readDataSet("DataTest/SetMembershipData.csv",5,20000));
+  gee.calculateGamma();
   std::cout << gee.Gamma << '\n';
 
   std::vector<double> VAL(std::begin(uval),std::end(uval));
   std::cout << gee.SetMemebership(VAL);
-  gee.readDataSet("DataTest/SetMembershipData.csv",3);
+
   return 0;
 }
